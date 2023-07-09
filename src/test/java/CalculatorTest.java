9c6d66c994753f8a61c3a82975a0c620fd924ae7
aneesh1512 <94799547+aneesh1512@users.noreply.github.com>
2023-07-09 06:07:30 +0000

    basic TDD

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
 
@@ -13,6 +14,29 @@ public class CalculatorTest {
         int actual = calculator.add(1, 2);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSub() {
+        int expected = -1;
+        int actual = calculator.subtract(3, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 4;
+        int actual = calculator.multiply(2, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDiv() {
+        // int expected = 0;
+        // int actual = calculator.divide(0, 0);
+
+        assertThrows(ArithmeticException.class, () -> calculator.divide(0, 0));
+        // assertEquals(expected, actual);
     }
 
 }
